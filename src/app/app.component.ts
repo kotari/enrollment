975505b56a9ba59b00eb3b2d30f4ff7b3b92aaaf
Rajesh Kotari <kotari@rajeshcentos.lan>
2017-11-27 07:58:35 -0600

    Initial copy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [EnrollmentService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+  ngOnInit() {
+   
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MsgsComponent } from './home/msgs/msgs.component';
+import { DependentsComponent } from './dependents/dependents.component';
+import { AddComponent } from './dependents/add/add.component';
+import { Http404Component } from './http-404/http-404.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard-service';
+import { CanDeactivateGuard } from './dependents/add/can-deactivate-guard.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    HomeComponent, 
+    MsgsComponent, 
+    DependentsComponent,
+    AddComponent, 
+    Http404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
